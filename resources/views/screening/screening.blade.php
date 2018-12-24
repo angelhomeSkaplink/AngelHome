9dc31fa4e61728a294210dfd2c30e360c453bea8
Bikram <bikram.skaplink@gmail.com>
2018-12-24 15:05:27 +0530

    screening NAMES

--- resources/views/screening/screening.blade.php
+++ resources/views/screening/screening.blade.php
@@ -50,12 +50,6 @@
     								<div class="autocomplete" style="width:200px;">
     									<input id="myInput" type="text" placeHolder="FUTURE RESIDENT">
     								</div>
-    								<!--<select name="id" id="id" class="form-control">
-    									<option value="">Select Future resident</option>
-    									@foreach ($crms_all as $prospect)
-    									<option value="{{ $prospect->id}}">{{ $prospect->pros_name }}</option>
-    									@endforeach
-    								</select>-->
     							</th>
     							<th class="th-position text-uppercase font-500 font-12">Phone No</th>
     							<th class="th-position text-uppercase font-500 font-12">
@@ -63,28 +57,31 @@
     									<input id="emailInput" type="text" placeHolder="EMAIL">
     								</div>
     							</th>
-    							<th class="th-position text-uppercase font-500 font-12">
+    							{{-- <th class="th-position text-uppercase font-500 font-12">
     								<div class="contactautocomplete" style="width:200px;">
     									<input id="contactInput" type="text" placeHolder="CONTACT PERSON">
     								</div>
-    							</th>
+    							</th> --}}
     							<th class="th-position text-uppercase font-500 font-12">Add Screening Records</th>
     							<th class="th-position text-uppercase font-500 font-12">View Screening Records</th>
     						</tr>
-    						@foreach ($crms as $crm)
+							@foreach ($crms as $crm)
+							@php
+								$n=explode(",",$crm->pros_name);
+							@endphp
     						<tr>
     							@if($crm->service_image == NULL)
     							<td><img src="hsfiles/public/img/538642-user_512x512.png" class="img-circle" width="40" height="40"></td>	
     							@else
     							<td><img src="hsfiles/public/img/{{ $crm->service_image }}" class="img-circle" width="40" height="40"></td>
     							@endif
-    							<td>{{ $crm->pros_name }}</td>
+    							<td>{{ $n[0] }} {{ $n[1] }} {{ $n[2] }}</td>
     							<?php 
     								$basic = DB::table('change_pross_record')->where([['pros_id', $crm->id], ['status', 1]])->first();{
     							?>
     							<td>{{ $basic->phone_p }}</td>
     							<td>{{ $basic->email_p }}</td>
-    							<td>{{ $basic->contact_person }}</td>
+    							{{-- <td>{{ $basic->contact_person }}</td> --}}
     							<?php } ?>
     							<td style="padding-left:70px"><a data-toggle="tooltip" data-placement="bottom" data-original-title="Add Screening Records" href="resposible_personal/{{ $crm->id }}"><i class="material-icons gray md-22"> add_circle</i></a></td>
     							<td style="padding-left:70px"><a data-toggle="tooltip" data-placement="bottom" data-original-title="View Screening Records" href="screening_view/{{ $crm->id }}"><i class="material-icons gray md-22"> visibility </i></a></td>
